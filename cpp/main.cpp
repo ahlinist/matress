022f450e8f6adfc5a1a476ccd6b07e67603a62e3
Anton Hlinisty <anton.hlinisty@gmail.com>
2023-12-15 14:58:57 +0100

    Externalize memory manager

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,17 +3,18 @@
 #include <memory>
 
 #include "content_formatter.hpp"
+#include "memory_manager.hpp"
 
 double determinant2x2(double** matrix);
 
-double determinant(double** matrix, int size);
-
-void cleanup(double** matrix, int size);
+double determinant(double** matrix, int size, std::shared_ptr<memory::MemoryManager> memory_manager);
 
 double** generate_sub_matrix(double** matrix, int size, int col_to_skip);
 
 int main() {
     std::shared_ptr<text::ContentFormatter> content_formatter = std::make_shared<text::ContentFormatterImpl>();
+    std::shared_ptr<memory::MemoryManager> memory_manager = std::make_shared<memory::MemoryManagerImpl>();
+
     int size = 4;
 
     double** matrix = new double*[size];
@@ -40,17 +41,17 @@ int main() {
     std::string matrix_str = content_formatter->format_matrix(matrix, size);
     std::cout << "matrix: \n" << matrix_str << std::endl;
 
-    double det = determinant(matrix, size);
+    double det = determinant(matrix, size, memory_manager);
     std::cout << "det: " << det << std::endl;
 
-    cleanup(matrix, size);
+    memory_manager->cleanup_matrix(matrix, size);
 }
 
 double determinant2x2(double** matrix) {
     return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
 }
 
-double determinant(double** matrix, int size) {
+double determinant(double** matrix, int size, std::shared_ptr<memory::MemoryManager> memory_manager/* to remove later*/) {
     if (size > 2) {
         int sign = 1;
         double result = 0;
@@ -58,8 +59,8 @@ double determinant(double** matrix, int size) {
 
         for (int col_to_skip = 0; col_to_skip < size; col_to_skip++) {
             double** sub_matrix = generate_sub_matrix(matrix, size, col_to_skip);
-            result += sign * matrix[0][col_to_skip] * determinant(sub_matrix, next_size);
-            cleanup(sub_matrix, next_size); //do we need to cleanup? mb yes bc of "new" kw?
+            result += sign * matrix[0][col_to_skip] * determinant(sub_matrix, next_size, memory_manager);
+            memory_manager->cleanup_matrix(sub_matrix, next_size); //do we need to cleanup? mb yes bc of "new" kw?
             sign = -sign;
         }
         
@@ -67,13 +68,6 @@ double determinant(double** matrix, int size) {
     } else {
         return determinant2x2(matrix);
     }
-}
-
-void cleanup(double** matrix, int size) {
-    for (int i = 0; i < size; ++i) {
-        delete[] matrix[i];
-    }
-    delete[] matrix;
 }
 
 double** generate_sub_matrix(double** matrix, int size, int col_to_skip) {
