8ff510f37a0e9202e33219d4a0689baac69ec77e
Anton Hlinisty <anton.hlinisty@gmail.com>
2023-12-19 14:19:54 +0100

    Add memory usage

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,3 +1,5 @@
+#include <sys/resource.h>
+
 #include <iostream>
 #include <string>
 #include <memory>
@@ -7,7 +9,11 @@
 #include "matrix_processor.hpp"
 #include "input_receiver.hpp"
 
+void print_memory_usage();
+
 int main(int argc, char *argv[]) {
+    print_memory_usage();
+
     std::shared_ptr<memory::MemoryManager> memory_manager = std::make_shared<memory::MemoryManagerImpl>();
     std::shared_ptr<matrix::MatrixProcessor> matrix_processor = std::make_shared<matrix::MatrixProcessorImpl>(memory_manager);
     std::shared_ptr<input::InputReceiver> input_receiver = std::make_shared<input::InputReceiverImpl>();
@@ -28,5 +34,13 @@ int main(int argc, char *argv[]) {
 
     std::cout << "det: " << det << std::endl;
 
+    print_memory_usage();
+    
     memory_manager->cleanup_matrix(matrix);
+}
+
+void print_memory_usage() {
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    std::cout << "Memory Usage: " << usage.ru_maxrss << " KB" << std::endl;
 }
